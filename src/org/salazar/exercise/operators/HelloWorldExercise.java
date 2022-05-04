ca8bc778cf938e8b26cff7bc0e18f077e4ae9e6f
Jose Manuel Salazar Nuñez <salazar.engineer@outlook.com>
2022-05-04 17:46:48 -0500

    Add more requirements and description on hello world

--- src/org/salazar/exercise/operators/HelloWorldExercise.java
+++ src/org/salazar/exercise/operators/HelloWorldExercise.java
@@ -4,11 +4,23 @@ import java.util.Scanner;
 
 public class HelloWorldExercise {
 
+	/**
+	 * Show a message in console ask for your name to show the phrase hello world
+	 * with your name
+	 */
 	public void printHelloWorld() {
-		String name=prompt("Please provide your name");
-		System.out.println("Hello world: "+name);
+		// Ask the user to provide the name and assign it to name variable
+		String name = prompt("Please provide your name");
+		// print the message Hello world plus the user name
+		System.out.println("Hello world: " + name);
 	}
 
+	/**
+	 * Show a message in the console and ask for user input
+	 * 
+	 * @param msg - The message to display in the console
+	 * @return the input from the user
+	 */
 	private String prompt(String msg) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println(msg);
